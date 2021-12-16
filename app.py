0cd2157f467216111eb07f407f5c79833a363552
48Lynn11 <89010222+48Lynn11@users.noreply.github.com>
2021-12-16 13:45:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def event_handle(event,json_line):
             headers = request.headers 
             json_headers = ({k:v for k, v in headers.items()}) 
             json_headers.update({'Host':'bots.dialogflow.com'}) 
-            url = ""https://dialogflow.cloud.google.com/v1/integrations/line/webhook/47832df3-f294-495a-abdb-c16a00b3e853 
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/47832df3-f294-495a-abdb-c16a00b3e853" 
             requests.post(url,data=json_line, headers=json_headers)
            replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
